546df65ed571d6c1513b0068c0df2c998ffbc013
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-09-04 23:35:37 +0300

    Created product and repo

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    @Test
+
+    public void removeExistingProductTest() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(32,"Тетрадь",200);
+        Product product2 = new Product(21, "Книга",750);
+        Product product3 = new Product(113, "Линейка",155);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(32);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product2, product3};
+    }
+}
